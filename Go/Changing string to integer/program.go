2fb6f210b393fd00f922378f832ec8fa84804a33
osi <45043329+osi-byte@users.noreply.github.com>
2020-09-07 12:43:34 +0630

    Create program.go

---
+++ Go/Changing string to integer/program.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	var i int = 42
+	fmt.Printf("%v , %T\n" , i, i)
+	
+	var j string 
+	j = strconv.Itoa(i)
+	fmt.Printf("%v, %T " , j , j)
+}
