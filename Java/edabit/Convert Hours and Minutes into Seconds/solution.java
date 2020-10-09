06b88658180af2902c39ce14060cb5ad09774536
Chan Nyein Wai <45043329+osi-byte@users.noreply.github.com>
2020-10-09 22:17:04 +0630

    Create solution.java

---
+++ Java/edabit/Convert Hours and Minutes into Seconds/solution.java
@@ -0,0 +1,8 @@
+public class Challenge {
+	public static int convert(int hours, int minutes) {
+		 int hour = 3600;
+		int minute = 60;
+		int result = (hour * hours) + (minute * minutes);
+			return result;
+  }
+}
