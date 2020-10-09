e5319b68294a49aba314dab76a9d382b45951629
Chan Nyein Wai <45043329+osi-byte@users.noreply.github.com>
2020-10-09 19:57:47 +0630

    Create solution.java

---
+++ Java/edabit/Return the Remainder from Two Numbers/solution.java
@@ -0,0 +1,9 @@
+public class Challenge {
+  public static int remainder(int a, int b) {
+		int result = a % b;
+		return result;
+  }
+	public static void main(String[] args) {
+		System.out.println(remainder(1,3));
+	}
+}
