4abb12e7d7491668d97c348118ed74aaa4ee1ad3
Chan Nyein Wai <45043329+osi-byte@users.noreply.github.com>
2020-10-09 20:39:35 +0630

    Create solution.java

---
+++ Java/edabit/Name Greeting!/solution.java
@@ -0,0 +1,9 @@
+public class Challenge {
+	public static String helloName(String name) {
+		String greet = "Hello " + name + "!";
+		return greet;
+	}
+	public static void main(String[] args) {
+		System.out.println(helloName("Gerald"));
+	}
+}
