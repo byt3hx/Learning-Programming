47c2c29c437eb71e1ebbcebbb216fa8b5186a133
Chan Nyein Wai <45043329+osi-byte@users.noreply.github.com>
2020-10-09 19:52:51 +0630

    Create solution.java

---
+++ Java/edabit/Return the Next Number from the Integer Passed/solution.java
@@ -0,0 +1,9 @@
+public class Challenge {
+	public static int addition(int num) {
+		int result = num + 1;
+		return result;
+	}
+	public static void main (String[] args){
+	System.out.println(addition(0));	
+	}
+}
