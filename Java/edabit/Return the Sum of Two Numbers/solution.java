81f4462668ef6afc3a4639cb97cf028751bb5531
Chan Nyein Wai <45043329+osi-byte@users.noreply.github.com>
2020-10-09 20:51:01 +0630

    Create solution.java

---
+++ Java/edabit/Return the Sum of Two Numbers/solution.java
@@ -0,0 +1,9 @@
+public class SumOfTwoNumbers {
+    public static int sum(int a, int b) {
+      int result = a + b;
+			return result;
+    }
+	public static void main(String[] args){
+		System.out.println(sum(3,2));
+	}
+}
